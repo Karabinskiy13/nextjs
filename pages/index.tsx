16655c1874bb54a73234a308302f5386ec9e6989
Karabin13 <ivan.karabinskiy1308@gmail.com>
2022-10-27 20:19:25 +0300

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 
 import { HomeStyle } from '../styles/Home';
 
 const Home = () => (
-  <HomeStyle>
-    <h1>NextJS Application</h1>
-  </HomeStyle>
+  <>
+    <HomeStyle>
+      <h1> NextJS</h1>
+    </HomeStyle>
+  </>
 );
 
 export default Home;
--- styles/Home.tsx
+++ styles/Home.tsx
@@ -7,3 +7,11 @@ export const HomeStyle = styled.div`
   align-items: center;
   color: #ffff1a;
 `;
+
+export const BlockStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  border: 1px solid linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5));
+`;
